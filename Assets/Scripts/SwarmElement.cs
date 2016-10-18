4bb7b17090a598db490802b0e009758add1b231b
TheFlow0360 <floriankochde@arcor.de>
2016-10-18 23:14:14 +0200

    Created new Unity3D project, added basic swarm behaviour scripts and test scene

---
+++ Assets/Scripts/SwarmElement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwarmElement : MonoBehaviour
+{
+    private Rigidbody body;
+
+    public float attractionFactor = 0.1f;
+    public float repulsionFactor1 = 20;
+    public float repulsionFactor2 = 15;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+	}
+
+    void OnEnable()
+    {
+        var manager = GetComponentInParent<SwarmManager>();
+        if (manager != null)
+        {
+            manager.RegisterElement(this);
+        }
+    }
+
+    void OnDestroy()
+    {
+        var manager = GetComponentInParent<SwarmManager>();
+        if (manager != null)
+        {
+            manager.UnregisterElement(this);
+        }
+    }
+	
+	void Update() {
+	    
+	}
+
+    public Vector3 GetPos()
+    {
+        return body.position;
+    }
+
+    public Vector3 GetTargetMovement(Vector3 target)
+    {
+        var movement = target - body.position;
+        movement *= attractionFactor - Mathf.Pow(repulsionFactor1, -Mathf.Pow(Vector3.Distance(target, body.position), 2) / repulsionFactor2);
+        //movement *= attractionFactor - repulsionFactor1/Mathf.Pow(Vector3.Distance(body.position, target),2);
+        return movement;
+    }
+
+    public void ApplyMovement(Vector3 movement)
+    {
+        body.velocity = movement;
+    }
+}
---
+++ Assets/Scripts/SwarmManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SwarmManager : MonoBehaviour
+{
+    private List<SwarmElement> elements = new List<SwarmElement>();
+
+    void Start () {
+
+	}
+	
+	void Update () {
+	    foreach (var element1 in elements)
+	    {
+            Vector3 movement = Vector3.zero;
+	        foreach (var element2 in elements)
+	        {
+	            if (element1 == element2)
+	            {
+	                continue;
+	            }
+	            movement += element1.GetTargetMovement(element2.GetPos());
+	        }
+	        element1.ApplyMovement(movement);
+	    }
+    }
+
+    public void RegisterElement(SwarmElement swarmElement)
+    {
+        elements.Add(swarmElement);
+    }
+
+    public void UnregisterElement(SwarmElement swarmElement)
+    {
+        elements.Remove(swarmElement);
+    }
+}
